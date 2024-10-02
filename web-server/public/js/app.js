a1c1f63e51416efb3136052b412559430ec46ee6
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-10-02 12:56:37 +0530

    weater app completed

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,14 +1,28 @@
 console.log('client side js file is loaded');
 
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error);
-        }
-        else {
-            console.log(data.location);
-            console.log(data.forecast);
-        }
+messageOne.textContent = 'Loading....';
+messageTwo.textContent = '';
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const location = search.value;
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            }
+            else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+            }
+        })
     })
+
 })
