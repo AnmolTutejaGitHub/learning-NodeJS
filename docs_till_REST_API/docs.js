53da038308fb7384dd71e5370740eb12cecebf3d
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-10-08 14:28:21 +0530

    created docs imp

--- docs_till_REST_API/docs.js
+++ docs_till_REST_API/docs.js
@@ -7,6 +7,10 @@
 //   }
 
 
+
+
+
+
 // server (uses routes)
 const express = require('express');
 const app = express();
@@ -20,6 +24,16 @@ app.use(express.json()); //will parse json data coming to object form
 // json data can be coming throught postman post request
 
 //////////////////////////////////////// without routes 
+// here res.send --> is sending json can also end html say for /home 
+app.get('/users', (req, res) => {
+    //mongoose queries // see docs
+    User.find({}).then((users) => { //ModelName.mongodb methods // find({}) returns all documents
+        res.send(users);
+    }).catch((e) => {
+        req.status(500).send(e);
+    })
+})
+
 app.post('/users', (req, res) => {
     // whatever is is req.body is saved as User document in my db
     const user = new User(req.body);
@@ -30,13 +44,47 @@ app.post('/users', (req, res) => {
     })
 })
 
-app.get('/users', (req, res) => {
-    //mongoose queries // see docs
-    User.find({}).then((users) => { //ModelName.mongodb methods // find({}) returns all documents
-        res.send(users);
-    }).catch((e) => {
-        req.status(500).send(e);
+
+// pathch --> update
+// whatever after /: is conssidered as params
+// postman : localhost:3000/users/67040f43ad3ead64bb0a9221
+// postman body : {"name" : "Anmol Tutej"}
+router.patch('/users/:id', async (req, res) => {
+
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const updates = Object.keys(req.body);
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update);
     })
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates' });
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    }
+    catch (e) {
+        res.status(400).send(e);
+    }
+})
+
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            res.status(404).send();
+        }
+        res.send(user);
+    } catch (e) {
+        res.status(500).send(e);
+    }
 })
 
 ///////////////////////////////////// using route // no need of app.get etc all of them defined in user route
@@ -45,6 +93,13 @@ app.use(userRoute);
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
 })
+
+
+
+
+
+
+
 
 
 
@@ -59,8 +114,19 @@ mongoose.connect('mongodb://127.0.0.1:27017/dbname');
 
 
 
+
+
+
+
+
+
+
+
+
+
 // user model
 const mongoose = require('mongoose');
+// mongodb will save this schema as users lowecasing 'User' and pluaring it 
 const User = mongoose.model('User', {
     name: {
         type: String,
@@ -94,6 +160,19 @@ module.exports = User;
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Routes > userRoute
 const express = require('express');
 const router = new express.Router();
@@ -116,4 +195,15 @@ module.exports = router;
 
 
 
+
+
+
+
+
+
+
+
+
+
+// to test our api in postman : GET localhost:3000/users
 // for mongodb docs referece go to mongodb not added here as will be using mongoose 
